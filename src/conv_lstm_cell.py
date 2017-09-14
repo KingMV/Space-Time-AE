cf0bfe17d7304582fef7bfead7030fcda2eed625
tnybny <tnybny@gmail.com>
2017-09-14 17:11:38 -0400

    implement barebones convLSTM cell, build up the AE net

--- src/conv_lstm_cell.py
+++ src/conv_lstm_cell.py
@@ -1,4 +1,45 @@
 import tensorflow as tf
 
 class ConvLSTMCell(tf.nn.rnn_cell.RNNCell):
-    def __init__(self):
+    def __init__(self, shape, num_filters, filter):
+        """
+        :param shape: (list) spatial dimensions [H, W]
+        :param num_filters: (int) number of output feature maps
+        :param filter: (list) dims of filter [F, F]
+        """
+        self.shape = shape
+        self.num_filters = num_filters
+        self.filter = filter
+        self.size = tf.TensorShape(shape + [self.num_filters])
+        self.feature_axis = self.size.ndims
+
+    def call(self, x, state):
+        """
+        Perform convLSTM cell ops given input at a given state
+        :param x: (tensor) input image of shape [batch_size, timesteps, H, W, channels]
+        :param state: (tuple) previous memory and hidden states of the cell
+        :return: new state after performing convLSTM ops given input and previous state
+        """
+        c, h = state
+
+        x = tf.concat([x, h], axis=self.feature_axis)
+        n = x.shape[-1]
+        m = 4 * self.num_filters if self.num_filters > 1 else 4
+        W = tf.get_variable('filter', self.filter + [n, m])
+        y = tf.nn.convolution(x, W, 'SAME')
+        y += tf.get_variable('bias', [m], initializer=tf.zeros_initializer())
+        j, i, f, o = tf.split(y, 4, axis=self.feature_axis)
+
+        f = tf.sigmoid(f)
+        i = tf.sigmoid(i)
+        c = c * f + i * tf.nn.tanh(j)
+
+        o = tf.sigmoid(o)
+        h = o * tf.nn.tanh(c)
+
+        state = tf.nn.rnn_cell.LSTMStateTuple(c, h)
+
+        return h, state
+
+
+
--- src/spatial_temporal_autoencoder.py
+++ src/spatial_temporal_autoencoder.py
@@ -1,91 +1,122 @@
 from __future__ import division, print_function, absolute_import
 
 import tensorflow as tf
-import numpy as np
 
 # network architecture definition
 NCHANNELS = 1
-conv1 = 128
-conv2 = 64
-deconv1 = 128
-deconv2 = 1
+CONV1 = 128
+CONV2 = 64
+DECONV1 = 128
+DECONV2 = 1
 WIDTH = 227
 HEIGHT = 227
 TVOL = 10
 
 class SpatialTemporalAutoencoder(object):
-    def __init__(self, alpha, batch_size, image_shape, time_steps, keep_prob):
-        self.x_ = tf.placeholder(tf.float32, [None, TVOL, HEIGHT, WIDTH, NCHANNELS])
-        self.y_ = tf.placeholder(tf.float32, [None, TVOL, HEIGHT, WIDTH, NCHANNELS]) # usually y_ = x_ if reconstruction
-        self.image_shape = image_shape
-
+    def __init__(self, alpha, batch_size, keep_prob):
+        self.x_ = tf.placeholder(tf.float32, [-1, TVOL, HEIGHT, WIDTH, NCHANNELS])
+        self.y_ = tf.placeholder(tf.float32, [-1, TVOL, HEIGHT, WIDTH, NCHANNELS]) # usually y_ = x_ if reconstruction
 
         self.batch_size = batch_size
         self.params = {
-            # 11x11 conv,  input, 32 output
             'c_w1': tf.Variable(tf.random_normal([11, 11, 1 , 128])),
             'c_b1': tf.Variable(tf.random_normal([128])),
             'c_w2': tf.Variable(tf.random_normal([5, 5, 128, 64])),
             'c_b2': tf.Variable(tf.random_normal([64])),
-            'c_w3': tf.Variable(tf.random_normal([5, 5,64, 128])),
+            'c_w3': tf.Variable(tf.random_normal([5, 5, 64, 128])),
             'c_b3': tf.Variable(tf.random_normal([128])),
             'c_w4': tf.Variable(tf.random_normal([11, 11, 128, 1])),
             'c_b4': tf.Variable(tf.random_normal([1]))
         }
 
-        self.y =
+        self.conved = self.spatial_encoder(self.x_)
+        self.z = self.temporal_encoder(self.conved)
+        self.deconvLSTMed = self.temporal_decoder(self.z)
+        self.y = self.spatial_decoder(self.deconvLSTMed)
 
-        self.reconstruction_loss = tf.reduce_mean(tf.pow(y_ - y, 2))
-        self.loss = self.reconstruction_loss
+        self.reconstruction_loss = tf.reduce_mean(tf.pow(self.y_ - self.y, 2))
+        self.regularization_loss = tf.constant(0)
+        self.loss = self.reconstruction_loss + self.regularization_loss
         self.optimizer = tf.train.AdamOptimizer(alpha).minimize(self.loss)
 
         self.sess = tf.InteractiveSession()
         self.sess.run(tf.global_variables_initializer())
 
     @staticmethod
-    def conv2d(x, w, b, activation, strides = 1):
+    def conv2d(x, w, b, activation = tf.nn.tanh, strides = 1):
         """
         Build a convolutional layer
-
         :param x: input
-        :param w: weight matrix
-        :param b: bia
+        :param w: filter
+        :param b: bias
         :param activation: activation func
         :param strides: the stride when filter is scanning through image
         :return: a convolutional layer representation
         """
-        x = tf.nn.conv2d(x, w, strides = [1, strides, strides, 1], padding='SAME')
+        x = tf.nn.conv2d(x, w, strides=[1, strides, strides, 1], padding='VALID')
+        x = tf.nn.bias_add(x, b)
+        return activation(x)
+
+    @staticmethod
+    def deconv2d(x, w, b, out_shape, activation=tf.nn.tanh, strides=1):
+        """
+        Build a deconvolutional layer
+        :param x: input
+        :param w: filter
+        :param b: bias
+        :param out_shape: shape of output tensor
+        :param activation: activation func
+        :param strides: the stride when filter is scanning
+        :return: a deconvolutional layer representation
+        """
+        x = tf.nn.conv2d_transpose(x, w, output_shape=out_shape, strides=[1, strides, strides, 1], padding='SAME')
         x = tf.nn.bias_add(x, b)
         return activation(x)
 
     def spatial_encoder(self, x):
         """
-        Build a spatial encoder network
-
-        :param x: tf tensor
-        :return:
+        Build a spatial encoder that performs convolutions
+        :param x: tensor of input image
+        :return: convolved representation
         """
-        h, w, c = self.image_shape
+        h, w, c = x.shape[2:]
         x = tf.reshape(x, shape=[-1, h, w, c])
-        conv1 = self.conv2d(x, self.params['c_w1'], self.params['c_b1'], activation=tf.nn.relu, strides=4)
-        conv2 = self.conv2d(conv1, self.params['c_w2'], self.params['c_b2'], activation=tf.nn.relu, strides=2)
-
+        conv1 = self.conv2d(x, self.params['c_w1'], self.params['c_b1'], activation=tf.nn.tanh, strides=4)
+        conv2 = self.conv2d(conv1, self.params['c_w2'], self.params['c_b2'], activation=tf.nn.tanh, strides=2)
         return conv2
 
     def spatial_decoder(self, x):
         """
-        Build a spatial decoder network
-        :param x:
-        :return:
+        Build a spatial decoder that performs deconvolutions on the input
+        :param x: tensor of some transformed representation of input
+        :return: deconvolved representation
         """
-        # revise this
-        conv3 = self.conv2d(x, self.params['c_w3'], self.params['c_b3'], activation=tf.nn.relu, strides=2)
-        conv4 = self.conv2d(conv3, self.params['c_w4'], self.params['c_b4'], activation=tf.nn.relu, strides=4)
+        h, w, c = x.shape[2:]
+        x = tf.reshape(x, shape=[-1, h, w, c])
+        deconv1 = self.deconv2d(x, self.params['c_w3'], self.params['c_b3'],
+                                [self.batch_size * TVOL, self.params['c_w3'].shape[3], 55, 55],
+                                activation=tf.nn.tanh, strides=2)
+        deconv2 = self.deconv2d(deconv1, self.params['c_w4'], self.params['c_b4'],
+                                [self.batch_size * TVOL, self.params['c_w4'].shape[3], HEIGHT, WIDTH],
+                                activation=tf.nn.tanh, strides=4)
+        return deconv2
 
-        return conv4
+    def temporal_encoder(self, x):
+        """
+        Build a temporal encoder that performs convLSTM encoding sequential operation
+        :param x: convolved representation of input volume
+        :return: convolved, convLSTMed representation
+        """
 
+        return x
 
+    def temporal_decoder(self, x):
+        """
+        Build a temporal decoder that performs convLSTM decoder sequential operation
+        :param x: convLSTMed representation
+        :return: convolved representation
+        """
+        return x
 
     def get_loss(self, x, y):
-        return self.loss.eval(feed_dict = {self.x_=y, self.y_ = y}, session = self.sess)
-
+        return self.loss.eval(feed_dict={self.x_: x, self.y_: y}, session=self.sess)
