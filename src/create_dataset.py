a7bf4f11f903cf8353b2336b770a9e921ce1f294
tnybny <tnybny@gmail.com>
2017-10-17 17:22:27 -0400

    sort result of os.walk

--- src/create_dataset.py
+++ src/create_dataset.py
@@ -4,13 +4,13 @@ from glob import glob
 import numpy as np
 from scipy.misc import imresize as resize
 
-train_dir = "/Users/tnybny/Documents/Anomaly detection in video/UCSD_Anomaly_Dataset.v1p2/UCSDped1/Train"
-test_dir = "/Users/tnybny/Documents/Anomaly detection in video/UCSD_Anomaly_Dataset.v1p2/UCSDped1/Test"
+train_dir = "../data/UCSD_Anomaly_Dataset.v1p2/UCSDped1/Train"
+test_dir = "../data/UCSD_Anomaly_Dataset.v1p2/UCSDped1/Test"
 
 # load images and resize to 227 x 227
-train = [resize(np.array(Image.open(y)), (227, 227)) for x in os.walk(train_dir) for y in sorted(
+train = [resize(np.array(Image.open(y)), (227, 227)) for x in sorted(list(os.walk(train_dir))) for y in sorted(
     glob(os.path.join(x[0], '*.tif')))]
-test = [resize(np.array(Image.open(y)), (227, 227)) for x in os.walk(test_dir) for y in sorted(
+test = [resize(np.array(Image.open(y)), (227, 227)) for x in sorted(list(os.walk(test_dir))) for y in sorted(
     glob(os.path.join(x[0], '*.tif')))]
 
 # rescale to [0, 1]
